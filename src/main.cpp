a63c6bf6587ba02315f229437a23524c30d15fbd
vophucthien <152169894+vophucthien@users.noreply.github.com>
2025-12-16 00:16:26 +0700

    Python image processing
    Python code for image processing and model prediction.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,51 @@
-#include <Arduino.h>
+from keras.models import load_model  # TensorFlow is required for Keras to work
+import cv2  # Install opencv-python
+import numpy as np
 
-void TaskLEDControl(void *pvParameters) {
-  pinMode(GPIO_NUM_48, OUTPUT); // Initialize LED pin
-  int ledState = 0;
-  while(1) {
-    
-    if (ledState == 0) {
-      digitalWrite(GPIO_NUM_48, HIGH); // Turn ON LED
-    } else {
-      digitalWrite(GPIO_NUM_48, LOW); // Turn OFF LED
-    }
-    ledState = 1 - ledState;
-    vTaskDelay(2000);
-  }
-}
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
 
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  xTaskCreate(TaskLEDControl, "LED Control", 2048, NULL, 2, NULL);
-}
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
 
-void loop() {
-  // Serial.println("Hello Custom Board");
-  // delay(1000);
-}
+# CAMERA can be 0 or 1 based on default camera of your computer
+camera = cv2.VideoCapture(0)
+
+while True:
+    # Grab the webcamera's image.
+    ret, image = camera.read()
+
+    # Resize the raw image into (224-height,224-width) pixels
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # Show the image in a window
+    cv2.imshow("Webcam Image", image)
+
+    # Make the image a numpy array and reshape it to the models input shape.
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    # Normalize the image array
+    image = (image / 127.5) - 1
+
+    # Predicts the model
+    prediction = model.predict(image)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    # Print prediction and confidence score
+    print("Class:", class_name[2:], end="")
+    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+    # Listen to the keyboard for presses.
+    keyboard_input = cv2.waitKey(1)
+
+    # 27 is the ASCII for the esc key on your keyboard.
+    if keyboard_input == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
